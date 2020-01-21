2203b7b5fce432e650532e2b270966a17c7c82b2
Edwin Espinoza <espined48@gmail.com>
2020-01-21 10:12:26 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/dogs", function(req, res){
+    res.render("index");
+});
+
+app.listen(3000, function(){
+    console.log("WildWoof app started!!!");
+});
